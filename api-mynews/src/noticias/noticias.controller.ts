162a7d87c2bacc86b1ca8ffbde2a8725b1333533
Gabriela Faria <gabs.s.faria78@gmail.com>
2023-06-15 23:43:38 +0000

    T002-GABI: feat - Criação das funcionalidade GET noticias, GET noticias especificas, POST cadastro de noticia e DELETE noticia

--- api-mynews/src/noticias/noticias.controller.ts
+++ api-mynews/src/noticias/noticias.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { NoticiasRepository } from './repository/noticias-repository';
+import { NoticiasCadastroDto } from './dto/noticias-cadastro.dto';
+import { NoticiasEntity} from './entity/noticias-entity';
+import { v4 as uuid } from 'uuid';
+
 
 @Controller('noticias')
-export class NoticiasController {}
+export class NoticiasController {
+    constructor(
+        private readonly _repository: NoticiasRepository
+    ){}
+
+    @Post()
+    createNoticia(@Body() noticia: NoticiasCadastroDto){
+        let noticiaEntity = new NoticiasEntity()
+
+
+        noticiaEntity.id = uuid()
+        noticiaEntity.titulo = noticia.titulo
+        noticiaEntity.subtitulo = noticia.subtitulo
+        noticiaEntity.autor = noticia.autor
+        noticiaEntity.data = noticia.data
+        noticiaEntity.imagens = noticia.imagens
+        noticiaEntity.destaque = noticia.destaque
+        noticiaEntity.resumo = noticia.resumo
+        noticiaEntity.texto = noticia.texto
+
+        this._repository.createNoticia(noticiaEntity)
+
+        return{
+            mensagem: 'Cadastro de noticias',
+            data: noticiaEntity
+        }
+
+    }
+
+
+    @Get()
+    getNoticias(){
+        return this._repository.listNoticias()
+    }
+
+    @Get(':id')
+    getNoticia(@Param() param: any){
+        return this._repository.findByID(param.id)        
+    }
+
+    @Delete(':id')
+    removeNoticia(@Param() param: any){
+        return this._repository.removeNoticia(param.id)
+    }
+}
