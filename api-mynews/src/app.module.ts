ec1816e24638806b6afd7b1f143b579095573fae
Gabriela Faria <gabs.s.faria78@gmail.com>
2023-06-15 00:48:56 +0000

    T001-GABI: feat - Criação do controller de noticias

--- api-mynews/src/app.module.ts
+++ api-mynews/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NoticiasController } from './noticias/noticias.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, NoticiasController],
   providers: [AppService],
 })
 export class AppModule {}
