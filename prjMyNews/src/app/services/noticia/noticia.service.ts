19936c1ed7da6be11ff439a81bcb3f8cc16f37c4
Jhonny Matheus de Oliveira <jhonnymatheus88@gmail.com>
2023-06-17 00:55:38 +0000

    T002-JHONNY: feat - criação da funcionalidade do noticia/service

--- prjMyNews/src/app/services/noticia/noticia.service.ts
+++ prjMyNews/src/app/services/noticia/noticia.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
+  private readonly URL = "https://3000-gabssfaria7-3jpamii2023-2ting6zah6e.ws-us100.gitpod.io"
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  buscarTodos(): Observable<any> {
+    return this.http.get(this.URL)
+  }
+
+  buscarUmaNoticia(id: string): Observable<any> {
+    return this.http.get(`${this.URL}/${id}`)
+  }
+
+  cadastrarNoticia(noticia: any): Observable<any> {
+    return this.http.post(this.URL, noticia)
+  }
+
+  excluirNoticia(id: string): Observable<any> {
+    return this.http.delete(`${this.URL}/${id}`)
+  }
 }
