02799b76e28aa514805c0dee6bbf8575189a1616
Olivio César <cesarbigs@gmail.com>
2023-06-17 01:32:21 +0000

    T001-OLIVIO: feat - criação da funcionalidade de cadastrar notícias

--- prjMyNews/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
+++ prjMyNews/src/app/cadastrar-noticia/cadastrar-noticia.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiaService } from '../services/noticia/noticia.service';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-cadastrar-noticia',
@@ -6,10 +9,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-noticia.page.scss'],
 })
 export class CadastrarNoticiaPage implements OnInit {
-
-  constructor() { }
+    titulo: string = ''
+    subtitulo: string = ''
+    autor: string =  ''
+    data: string = ''
+    imagens: [] = []
+    destaque: string = ''
+    resumo: string = ''
+    texto: string = ''
+  constructor(
+    private readonly service: NoticiaService,
+    private readonly alert: AlertController
+  ) { }
 
   ngOnInit() {
   }
+  async cadastrar() {
+    let noticia = {
+      titulo: this.titulo,
+      subtitulo: this.subtitulo,
+      autor: this.autor,
+      data: this.data,
+      imagens: this.imagens,
+      destaque: this.destaque,
+      resumo: this.resumo,
+      texto: this.texto
 
+    }
+
+    this.service.cadastrarNoticia(noticia).subscribe({
+      next: async (dados: any) => {
+        console.log(dados)
+
+        const mensagem = await this.alert.create({
+          header: 'Mensagem',
+          message: 'Notícia cadastrada com sucesso!',
+          buttons: ['OK'],
+        });
+    
+        await mensagem.present();
+      },
+      error: async (error: any) => {
+        console.error(error)
+
+        let erros = ''
+
+        error.error.message.forEach(
+          (erro: any) => erros += erro + '<br>'
+        )
+
+        const mensagem = await this.alert.create({
+          header: 'Mensagem',
+          message: erros,
+          buttons: ['OK']
+        });
+    
+        await mensagem.present();
+      }
+    })
+  }
 }
